55bff4c5ca145b73e46b6fc1f17d9d885ebb5a78
ezio84 <brabus84@gmail.com>
2018-08-17 02:31:10 -0700

    CF: Allow to doubletap or longpress power to toggle flashlight [2/3]
    Adin's edit: added toast notifying jump to camera disable
    
    Change-Id: I9282e2d2426af5901b5e31895b5e4ce3479b7b20
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- src/org/devheadz/dhtweaks/fragments/buttons/Buttons.java
+++ src/org/devheadz/dhtweaks/fragments/buttons/Buttons.java
@@ -17,17 +17,67 @@
 package org.devheadz.dhtweaks.fragments.buttons;
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.widget.Toast;
 
+import com.android.internal.util.cr.CrUtils;
 import com.android.settings.R;
 import com.android.settings.CustomSettingsPreferenceFragment;
 
-public class Buttons extends CustomSettingsPreferenceFragment {
+public class Buttons extends CustomSettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
     private static final String TAG = "Buttons";
+    private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
+
+    private ListPreference mTorchPowerButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.buttons);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        if (!CrUtils.deviceSupportsFlashLight(getContext())) {
+            Preference toRemove = prefSet.findPreference(TORCH_POWER_BUTTON_GESTURE);
+            if (toRemove != null) {
+                prefSet.removePreference(toRemove);
+            }
+        } else {
+            mTorchPowerButton = (ListPreference) findPreference(TORCH_POWER_BUTTON_GESTURE);
+            int mTorchPowerButtonValue = Settings.Secure.getInt(getActivity().getContentResolver(),
+                    Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0);
+            mTorchPowerButton.setValue(Integer.toString(mTorchPowerButtonValue));
+            mTorchPowerButton.setSummary(mTorchPowerButton.getEntry());
+            mTorchPowerButton.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        boolean DoubleTapPowerGesture = Settings.Secure.getInt(getActivity().getContentResolver(),
+                Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1) == 0;
+        if (preference == mTorchPowerButton) {
+            int mTorchPowerButtonValue = Integer.valueOf((String) objValue);
+            int index = mTorchPowerButton.findIndexOfValue((String) objValue);
+            mTorchPowerButton.setSummary(
+                    mTorchPowerButton.getEntries()[index]);
+            Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.TORCH_POWER_BUTTON_GESTURE,
+                    mTorchPowerButtonValue);
+            if (mTorchPowerButtonValue == 1 && DoubleTapPowerGesture) {
+                //if doubletap for torch is enabled, switch off double tap for camera
+                Settings.Secure.putInt(getActivity().getContentResolver(),
+                        Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1);
+                Toast.makeText(getActivity(),
+                    (R.string.torch_power_button_gesture_dt_toast),
+                    Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        return false;
     }
 }
